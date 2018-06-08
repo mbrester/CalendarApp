ca0d3693f3c034fa1bb647fd6fcc2325d77a9502
mitch brester <shremp@yahoo.com>
2018-06-07 19:08:12 -0500

    Initial Commit

---
+++ MikesCalendarApp2/Controllers/HomeController.cs
@@ -0,0 +1,179 @@
+﻿using Google.Apis.Calendar.v3.Data;
+using MikesCalendarApp2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MikesCalendarApp2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static class Globals
+        {
+            public const Int32 BUFFER_SIZE = 10; // Unmodifiable
+            public static String FILE_NAME = "Output.txt"; // Modifiable
+            public static readonly String CODE_PREFIX = "US-"; // Unmodifiable
+            public static DateTime date = DateTime.Now;
+            public static int month = date.Month;
+
+
+        }
+        
+
+        public ActionResult Index()
+        {
+            if (Request.QueryString["month"] != null)
+            {
+                if (Request.QueryString["month"].ToString() == "-1")
+                {
+                    Globals.month = Globals.month - 1;
+                }
+                if (Request.QueryString["month"].ToString() == "1")
+                {
+                    Globals.month = Globals.month + 1;
+                }
+            }
+            else
+            {               
+                   int  month2 = Globals.date.Month;
+                    Globals.month = month2;
+            }
+            GoogleAPI eventGetter = new GoogleAPI();
+            Events events = new Events();           
+            events = eventGetter.GetEvents(Globals.month);
+            ViewBag.currentMonth = Globals.month;
+            ViewBag.events = events;
+            return View();
+        }
+
+        public ActionResult EventDetails()
+        {
+            if (Request.QueryString["eventID"] != null)
+            {
+                HttpCookie myCookie = new HttpCookie("EventId");
+                myCookie.Value = Request.QueryString["eventID"].ToString();
+                myCookie.Expires = DateTime.Now.AddDays(1d);
+                Response.Cookies.Add(myCookie);
+                //Redirect to a new view, so we don't have an ugly event id in the url, and I am sure there are seceraty risks too.
+                //Also the selected event is now saved as a cookie.
+                return RedirectToAction("EventDetail", "Home");
+            }
+            else
+            {
+                return RedirectToAction("Index","Home");
+            }
+            //Will never hit this.    
+            return View();
+        }
+
+        public ActionResult AddEvent()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddEvent(string startDate, string endDate, string name, string details)
+        {
+            GoogleAPI eventCreater = new GoogleAPI();
+
+            DateTime start = Convert.ToDateTime(startDate);
+            DateTime end = Convert.ToDateTime(endDate);
+
+            bool results = eventCreater.CreateEvent(start, end, name, details);
+            if (results)
+            {
+                ViewBag.results = "Event Added";
+            }
+            else
+            {
+                ViewBag.results = "Something went wrong.";
+            }
+            return View();
+        }
+
+        public ActionResult EventDetail()
+        {
+            Event e = new Event();
+            if (Request.Cookies["EventId"] != null)
+            {
+                string eventId;         
+                { eventId = Request.Cookies["EventId"].Value.ToString(); }
+                GoogleAPI eventGetter = new GoogleAPI();
+                
+                e = eventGetter.GetEventById(eventId);
+            }
+            ViewBag.e = e;
+            return View();
+        }
+        
+        public ActionResult EventEdit()
+        {
+            Event e = new Event();
+            if (Request.Cookies["EventId"] != null)
+            {
+                string eventId;
+                { eventId = Request.Cookies["EventId"].Value.ToString(); }
+                GoogleAPI eventGetter = new GoogleAPI();
+
+                e = eventGetter.GetEventById(eventId);
+            }
+            ViewBag.e = e;
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult EventEdit(string startDate, string endDate, string name, string details)
+        {
+            GoogleAPI eventEditor = new GoogleAPI();
+
+            DateTime start = Convert.ToDateTime(startDate);
+            DateTime end = Convert.ToDateTime(endDate);
+            string eventId;
+            if (Request.Cookies["EventId"] != null)
+            {
+
+                { eventId = Request.Cookies["EventId"].Value.ToString(); }
+                bool results = eventEditor.EditEvent(start, end, name, details, eventId);
+
+
+                if (results)
+                {
+                    ViewBag.results = "Event Edited";
+
+                }
+                else
+                {
+                    ViewBag.results = "Something went wrong.";
+                }
+                Event e = new Event();
+                if (Request.Cookies["EventId"] != null)
+                {
+                    GoogleAPI eventGetter = new GoogleAPI();
+
+                    e = eventGetter.GetEventById(eventId);
+                    ViewBag.e = e;
+                }
+
+            }
+            else
+            {
+                ViewBag.results = "Something went wrong.";
+            }
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MikesCalendarApp2/Models/GoogleAPI.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System.IO;
+using System.Threading;
+
+namespace MikesCalendarApp2.Models
+{
+    public class GoogleAPI
+    {
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/calendar-dotnet-quickstart.json
+        static string[] Scopes = { CalendarService.Scope.Calendar };
+        static string ApplicationName = "Google Calendar API .NET Quickstart";
+
+        public Events GetEvents(int month)
+        {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream(System.Web.Hosting.HostingEnvironment.MapPath("~\\client_secret.json"), FileMode.Open, FileAccess.Read))
+            {
+                string credPath = "";
+                credPath = Path.Combine(credPath, System.Web.Hosting.HostingEnvironment.MapPath("~/.credentials/calendar-dotnet-quickstart.json"));
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Calendar API service.
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define parameters of request.
+            EventsResource.ListRequest request = service.Events.List("primary");
+            DateTime date = new DateTime();
+            date = DateTime.Now;
+            DateTime monthDate = new DateTime(date.Year, month, 1);
+            request.TimeMin = monthDate;
+            DateTime endOfMonth = new DateTime(date.Year,
+                                   month,
+                                   DateTime.DaysInMonth(date.Year,
+                                                        month));
+            request.TimeMax = endOfMonth;
+            request.ShowDeleted = false;
+            request.SingleEvents = true;
+            request.MaxResults = 31;
+            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+            
+            // List events.
+            Events events = request.Execute();
+            return events;
+        }
+        public Event GetEventById(string id)
+        {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream(System.Web.Hosting.HostingEnvironment.MapPath("~\\client_secret.json"), FileMode.Open, FileAccess.Read))
+            {
+                string credPath = "";
+                credPath = Path.Combine(credPath, System.Web.Hosting.HostingEnvironment.MapPath("~/.credentials/calendar-dotnet-quickstart.json"));
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Calendar API service.
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define parameters of request.
+            EventsResource.GetRequest request = service.Events.Get("primary",id);
+            DateTime date = new DateTime();
+            date = DateTime.Now;
+         
+            // List events.
+            Event events = request.Execute();
+            return events;
+        }
+
+        public bool CreateEvent(DateTime start, DateTime end, string name,string details)
+        {
+            try
+            {
+                Event e = new Event();
+                EventDateTime startDate = new EventDateTime();
+                EventDateTime endDate = new EventDateTime();
+                startDate.DateTime = start;
+                endDate.DateTime = end;
+
+                e.Summary = name;
+                e.Start = startDate;
+                e.End = endDate;
+                e.Description = details;
+
+                UserCredential credential;
+
+                using (var stream =
+                    new FileStream(System.Web.Hosting.HostingEnvironment.MapPath("~\\client_secret.json"), FileMode.Open, FileAccess.Read))
+                {
+                    string credPath = "";
+                    credPath = Path.Combine(credPath, System.Web.Hosting.HostingEnvironment.MapPath("~/.credentials/calendar-dotnet-quickstart.json"));
+
+                    credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                        GoogleClientSecrets.Load(stream).Secrets,
+                        Scopes,
+                        "user",
+                        CancellationToken.None,
+                        new FileDataStore(credPath, true)).Result;
+                    Console.WriteLine("Credential file saved to: " + credPath);
+                }
+
+                // Create Google Calendar API service.
+                var service = new CalendarService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = ApplicationName,
+                });
+
+                // Define parameters of request.
+                EventsResource.InsertRequest request = service.Events.Insert(e, "primary");
+                request.Execute();
+
+
+                return true;
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+        }
+
+        public bool EditEvent(DateTime start, DateTime end, string name, string details,string id)
+        {
+            try
+            {
+                Event e = new Event();
+                EventDateTime startDate = new EventDateTime();
+                EventDateTime endDate = new EventDateTime();
+                startDate.DateTime = start;
+                endDate.DateTime = end;
+
+                e.Summary = name;
+                e.Start = startDate;
+                e.End = endDate;
+                e.Description = details;
+
+                UserCredential credential;
+
+                using (var stream =
+                    new FileStream(System.Web.Hosting.HostingEnvironment.MapPath("~\\client_secret.json"), FileMode.Open, FileAccess.Read))
+                {
+                    string credPath = "";
+                    credPath = Path.Combine(credPath, System.Web.Hosting.HostingEnvironment.MapPath("~/.credentials/calendar-dotnet-quickstart.json"));
+
+                    credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                        GoogleClientSecrets.Load(stream).Secrets,
+                        Scopes,
+                        "user",
+                        CancellationToken.None,
+                        new FileDataStore(credPath, true)).Result;
+                    Console.WriteLine("Credential file saved to: " + credPath);
+                }
+
+                // Create Google Calendar API service.
+                var service = new CalendarService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = ApplicationName,
+                });
+
+                // Define parameters of request.
+                EventsResource.UpdateRequest request = service.Events.Update(e, "primary",id);
+                request.Execute();
+
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
